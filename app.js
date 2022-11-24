3ef39b169923a364f97e10c5c8a3c1afd943a923
ying <34480@ma-web.nl>
2022-11-24 10:13:41 +0100

    Added index html + app js files

---
+++ app.js
@@ -0,0 +1,19 @@
+class app 
+{
+    runapplication()
+    {
+        console.log("Hello World")
+        const appNaam("Hello kitty")
+        let versienummer()
+        let versiedatum("24/11/2022")
+        const autheur("Ying2406")
+        const copyright("Ying2406")
+        const distributeur("Ying2406")
+        const darkmode()
+    }
+
+}
+
+let app = new app();
+app.runapplication();
+
