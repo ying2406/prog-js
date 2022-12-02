d391c23788d5091f36e7f5e9a828848dd9db26cf
ying <34480@ma-web.nl>
2022-12-02 15:01:43 +0100

    Modified canvus code

--- canvuis/app.js
+++ canvuis/app.js
@@ -1,61 +1,70 @@
-class App
-{
-    runapplication()
-    {
-        let canvas = document.getElementById("CanvasId");
-        let g = canvas.getContext("2d");
-        g.fillRect(0,0,10,10)
+var canvas = document.getElementById('CanvasId');
+var g = canvas.getContext('2d');
+g.canvas.width  = window.innerWidth;
+g.canvas.height = window.innerHeight;
+var gX = canvas.width / 2;
+var gY = canvas.height / 2;
+var black = "#735F32";
+var white = "#FFF";
 
-        //g.beginPath();
-        //g.moveTo(20, 20);
-        //g.lineTo(300, 300);
-        //g.closePath();
-        //g.stroke();
+//house
+g.beginPath();
+g.fillStyle = "pink";
+g.moveTo(300,100);
+g.lineTo(700,200);
+g.lineTo(600,400);
+g.lineTo(200,300);
+g.lineTo(300,100);
+g.fill();
+g.stroke();
+g.closePath();
 
-        //g.beginPath();
-        //g.fillStyle = "yellow";
-        //g.moveTo(20, 20);
-        //g.lineTo(300, 300);
-        //g.lineTo(580, 20);
-        //g.closePath();
-        //g.stroke();
-        //g.fill();
+g.beginPath();
+g.fillStyle = "pink";
+g.moveTo(200,300);
+g.lineTo(200,500);
+g.lineTo(600,600);
+g.lineTo(600,400);
+g.lineTo(200,300);
+g.fill();
+g.stroke();
+g.closePath();
 
-        g.beginPath();
-        g.fillStyle = "yellow";
-        g.lineTo(30, 10);
-        g.lineTo(20, 30);
-        g.lineTo(60, 40);
-        g.lineTo(70, 20);
-        g.closePath();
-        g.stroke();
-        g.fill();
-        
-        g.beginPath();
-        g.fillStyle = "red";
-        g.lineTo(20, 30);
-        g.lineTo(20, 50);
-        g.lineTo(60, 60);
-        g.lineTo(60, 40);
-        g.closePath();
-        g.stroke();
-        g.fill();
+g.beginPath();
+g.fillStyle = "pink";
+g.moveTo(600, 400);
+g.lineTo(600, 600);
+g.lineTo(800, 500);
+g.lineTo(800, 300);
+g.closePath();
+g.stroke();
+g.fill();
 
-        g.beginPath();
-        g.fillStyle = "blue";
-        g.moveTo(60, 40);
-        g.lineTo(60, 60);
-        g.lineTo(80, 50);
-        g.lineTo(80, 30);
-        g.lineTo(70, 20);
-        g.lineTo(60, 40);
-        g.closePath();
-        g.stroke();
-        g.fill();
-        console.log(canvas);
+g.beginPath();
+g.moveTo(800, 300);
+g.lineTo(700, 200)
+g.lineTo(600, 400);
+g.closePath();
+g.stroke();
+g.fill();
 
-    }
-}
+//door
+g.beginPath(); 
+g.moveTo(600, 500);
+g.lineTo(650, 470);
+g.lineTo(650, 575)
+g.lineTo(650, 470)
+g.closePath();
+g.stroke();
+g.fill();
 
-let app = new App();
-app.runapplication();
+//raam 
+g.beginPath(); 
+g.fillStyle = "magenta";
+g.moveTo(275, 450);
+g.lineTo(450, 490);
+g.lineTo(450, 400)
+g.lineTo(290, 365)
+g.closePath();
+g.stroke();
+g.fill();
