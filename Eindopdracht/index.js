7aafdbe998f279948fd0033c700b3d1b9bb94b86
ying <34480@ma-web.nl>
2023-01-26 00:34:49 +0100

    Modified files

--- Eindopdracht/index.js
+++ Eindopdracht/index.js
@@ -1,82 +1,91 @@
 var canvas = document.getElementById('CanvasId');
-var g = canvas.getContext('2d');
-g.canvas.width  = window.innerWidth;
-g.canvas.height = window.innerHeight;
-var gX = canvas.width / 2;
-var gY = canvas.height / 2;
-var black = "#735F32";
-var white = "#FFF";
+var ctx = canvas.getContext('2d');
 
-canvas.width = 400;
-canvas.height = 300;
-
-// Draw the card shape
-g.beginPath();
-g.moveTo(50, 50);
-g.lineTo(350, 50);
-g.lineTo(350, 250);
-g.lineTo(50, 250);
-g.lineTo(50, 50);
-g.closePath();
-g.stroke();
-
-//house
-g.beginPath();
-g.fillStyle = "pink";
-g.moveTo(30,10);
-g.lineTo(70,20);
-g.lineTo(60,60);
-g.lineTo(20,30);
-g.lineTo(30,10);
-g.fill();
-g.stroke();
-g.closePath();
-
-g.beginPath();
-g.fillStyle = "pink";
-g.moveTo(20,30);
-g.lineTo(20,50);
-g.lineTo(60,60);
-g.lineTo(60,40);
-g.lineTo(20,30);
-g.fill();
-g.stroke();
-g.closePath();
-
-g.beginPath();
-g.fillStyle = "pink";
-g.moveTo(60, 40);
-g.lineTo(60, 60);
-g.lineTo(80, 50);
-g.lineTo(80, 30);
-g.closePath();
-g.stroke();
-g.fill();
-
-g.beginPath();
-g.moveTo(80, 30);
-g.lineTo(70, 20)
-g.lineTo(60, 40);
-g.closePath();
-g.stroke();
-g.fill();
-
-//boom
-g.fillStyle = "rgb(0, 100, 0)";
-
-g.fillRect(20, 35, 10, 20);
-
-g.beginPath();
-g.arc(25, 25, 15, 0, 2 * Math.PI);
-g.fill();
-
-//house 
-g.moveTo(x+25, y+70);
-g.moveTo(x+40, y+65);
-g.moveTo(x+85, y+10);
-g.stroke();
-g.fill();
-
-// Draw multiple houses
-for (let i = 0; i < 5; i++) {
+function Achtergrond(){
+    var ctx = canvas.getContext("2d");
+    var gradient = ctx.createLinearGradient(400, 0, 400, 300);
+    gradient.addColorStop(0, 'pink');
+    gradient.addColorStop(1, 'white');
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, 400, 300);
 }
+
+function house(x, y){ 
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    ctx.moveTo(x,y);
+    ctx.lineTo(x+40,y+10);
+    ctx.lineTo(x+30,y+50);
+    ctx.lineTo(x-10,y+20);
+    ctx.lineTo(x,y);
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.fillStyle = "orange";
+    ctx.moveTo(x-10,y+20);
+    ctx.lineTo(x-10,y+40);
+    ctx.lineTo(x+30,y+50);
+    ctx.lineTo(x+30,y+30);
+    ctx.lineTo(x-10,y+20);
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.fillStyle = "gray";
+    ctx.moveTo(x+30,y+30);
+    ctx.lineTo(x+30, y+50);
+    ctx.lineTo(x+50, y+40);
+    ctx.lineTo(x+50, y+20);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(x+50, y+20);
+    ctx.lineTo(x+40, y+10)
+    ctx.lineTo(x+30, y+30);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+}
+
+function boom(x, y){
+ctx.fillStyle = "rgb(165,42,42)"
+
+ctx.fillRect(x, y, 10, 25);
+
+ctx.fillStyle = "#013220";
+
+ctx.beginPath();
+ctx.arc(x+5, y-10, 15, 0, 2 * Math.PI);
+ctx.fill();
+}
+
+Achtergrond();
+
+
+
+function Straat(x, y){
+ctx.fillStyle = "black";
+ctx.font = "30px arial";
+ctx.fillText("Happy Holidays", 150, 100);
+//multiple house+boom 
+    for (let i = 0; i < 8; i++) {
+        house(x, y);
+
+        house(x+50, y+10);
+
+        boom(x+10, y+80);
+        boom(x+80, y+90);
+        boom(x+50, y+100);
+        
+        x = x+100
+        y = y+15
+    }
+}
+
+Straat(0, 100);
+Straat(-10, 220);
